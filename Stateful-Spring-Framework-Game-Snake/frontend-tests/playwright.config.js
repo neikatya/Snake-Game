2a7dfd74f52f54197d6a6c6fc41bade5307fc448
neikatya <cat05062001@gmail.com>
2026-04-14 14:44:16 +0300

    Update playwright.config.js

--- Stateful-Spring-Framework-Game-Snake/frontend-tests/playwright.config.js
+++ Stateful-Spring-Framework-Game-Snake/frontend-tests/playwright.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require('@playwright/test');
+
 module.exports = defineConfig({
     testDir: './tests',
+    timeout: 30000,
     use: {
         baseURL: 'http://localhost:8080',
         browserName: 'chromium',
-        headless: false
+        headless: true,
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
     },
+    fullyParallel: false,
+    workers: 1,
 });
